2cf3dde6e097fe34887608c70c3700b02d851008
ensieh <Ensieh@example.com>
2024-01-31 15:17:34 +0100

    initial code

---
+++ index.js
@@ -0,0 +1,12 @@
+//  .test {
+// background - color: red;
+/*  width: 1000px; */
+/* max-width: 1000px; */
+
+// max-width: 50rem;
+/* 1 rem=16px -> 50rem  (this is by default 1 rem is 16 if the user do not change the font size*/
+
+// padding: 4rem;
+// color: #fff;
+// font - size: 2rem;
+// } 
